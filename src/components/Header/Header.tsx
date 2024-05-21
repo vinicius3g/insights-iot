cd7321343b45ca4b762f564d55b65914617b57e8
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2024-05-21 18:07:02 -0300

    feat(Header): header da aplicaçao com opçao de logout e exibiçao de usuario logado

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import styles from "./Header.module.css";
+import { useAuth } from "../../context/AuthContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+  const { isAuthenticated, logout } = useAuth();
+  const navigate = useNavigate();
+  const [user, setUser] = useState({} as any);
+
+  const token = localStorage.getItem("token");
+
+  const getUser = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/api/users/profile", {
+        headers: { Authorization: `BEARER ${token}` },
+      });
+
+      setUser(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, [isAuthenticated]);
+
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
+
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>Insights Iot</h1>
+      {isAuthenticated && (
+        <div className={styles.user_info_wrapper}>
+          <div>
+            <p className={styles.user_info}>
+              bem vindo! <br />
+              <span className={styles.user_name}>{user?.username}</span>
+            </p>
+          </div>
+          <button className={styles.logout} onClick={handleLogout}>sair</button>
+        </div>
+      )}
+    </header>
+  );
+};
+
+export default Header;
